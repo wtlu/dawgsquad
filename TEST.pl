ce1579632c430c206c2286c36653e3a89fba51cc
Greg Brandt <brandtg@uw.edu>
2011-04-27 23:21:13 -0700

    updates automated testing

--- TEST.pl
+++ TEST.pl
@@ -6,8 +6,8 @@ use strict;
 use File::Spec;
 
 # build test log filename (without spaces)
-my $time = localtime;
-$time =~ s/ /-/g;
+my ($min,$hour,$mday,$mon,$year) = (localtime)[1,2,3,4,5];
+my $time = ($mon+1) . '-' . ($mday) . '-' . ($year+1900) . "_" . $hour . '-' . $min;
 my $log_file = File::Spec->catfile(File::Spec->curdir(), 'TEST_LOGS', $time);
 open LOG, ">$log_file";
 
@@ -21,12 +21,17 @@ my $test_cmd = File::Spec->catfile(File::Spec->updir(), 'cake', 'console', 'cake
 chdir File::Spec->catfile('sharingmedia', 'app');
 
 # run all tests and write to log file
-while (<TEST_CASES>) {
-  chomp;
-  if (!/^\#/ && !/^\s*$/) {	# not comment or blank line
-    # run the test
-    my $output = `$test_cmd testsuite app case $_`;
-    print LOG $output;
+if ($ARGV[0] eq '-a') {		# run all
+  my $output = `$test_cmd testsuite app all`;
+  print LOG $output;
+} else {
+  while (<TEST_CASES>) {
+    chomp;
+    if (!/^\#/ && !/^\s*$/) {	# not comment or blank line
+      # run the test
+      my $output = `$test_cmd testsuite app case $_`;
+      print LOG $output;
+    }
   }
 }
 
