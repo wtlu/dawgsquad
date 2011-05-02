abdb00eb5fa760a205fca1bf23f6e8c33bbdfa3a
Greg Brandt <brandtg@uw.edu>
2011-05-01 22:33:41 -0700

    cleaned up TEST.pl

--- tools/TEST.pl
+++ tools/TEST.pl
@@ -1,36 +1,79 @@
 #!/usr/bin/perl -w
-
-# This program runs all tests and stores them in a log file
-# whose name is the day and time that the tests were run
 use strict;
+use Cwd;
 use File::Spec;
 use Net::SMTP;
 
-# build test log filename (without spaces)
+# Author: Greg Brandt
+# Purpose: Runs SharingMedia test suite
+# Comments:
+#  - assumes that script is in REPOS/tools directory
+
+# vars
+my $APP_NAME = 'sharingmedia';
+my $LOG_DIR_NAME = 'TEST_LOGS';
+my $EMAIL_RECIPIENT = 'weiting.t.lu@gmail.com';
+my $TOOL_DIR = 'tools';
+
+# check args
+die "Usage: run_test.pl [-a|-f] [file]" if @ARGV != 1 && @ARGV != 2;
+
+# check current directory
+die "Wrong directory: move to REPOS/$TOOL_DIR" if Cwd::cwd() !~ /($TOOL_DIR)$/;
+
+# determine what to test
+my $fname;
+my $all = 0;
+
+# run all tests
+if ($ARGV[0] eq '-a') {
+  $all = 1;
+}
+# run subset of tests
+elsif ($ARGV[0] eq '-f') {
+  die "Usage: no filename specified" if !defined($ARGV[1]);
+  open TESTS, "<$ARGV[1]" or die "Couldn't open $ARGV[1]";
+}
+# incorrect usage
+else {
+  die "Usage: $ARGV[0] invalid flag";
+}
+
+# build test log filename
 my ($min,$hour,$mday,$mon,$year) = (localtime)[1,2,3,4,5];
-my $time = ($mon+1) . '-' . ($mday) . '-' . ($year+1900) . "_" . $hour . '-' . $min;
-open LOG, ">" . File::Spec->catfile(File::Spec->updir(), 'TEST_LOGS', $time);
+my $time = ($mon+1) . '-' . ($mday) . '-' . ($year+1900) . '_' . $hour . '-' . $min;
 
-# get test cases
-my @tests = ();
-open TEST_CASES, "<TEST_CASES.txt"
-  or die "couldn't open TEST_CASES.txt";
+# create log directory if it doesn't exist
+my $log_dir = File::Spec->catfile(File::Spec->updir(), $LOG_DIR_NAME);
+mkdir $log_dir if (! -d $log_dir);
 
-# build the test command
-my $test_cmd = File::Spec->catfile(File::Spec->updir(), 'cake', 'console', 'cake');
-chdir File::Spec->catfile(File::Spec->updir(), 'sharingmedia', 'app');
+# open time-stamped log file for running tests
+open LOG, '>' . File::Spec->catfile($log_dir, $time) . '.log';
 
-# run all tests and write to log file
+# change to appropriate directory to run tests
+chdir File::Spec->catfile(File::Spec->updir(), $APP_NAME, 'app');
+
+# build test command
+my $cmd = File::Spec->catfile(File::Spec->updir(), 'cake', 'console', 'cake');
+if ($all) {
+  $cmd .= ' testsuite app all';
+} else {			# subset
+  $cmd .= ' testsuite app case';
+}
+
+# run tests and write to log
 my $msg;
-if (defined($ARGV[0]) && $ARGV[0] eq '-a') {		# run all
-  my $output = `$test_cmd testsuite app all`;
+my $output;
+if ($all) {
+  $output = `$cmd`;
+  $msg .= $output;
   print LOG $output;
 } else {
-  while (<TEST_CASES>) {
+  while (<TESTS>) {
     chomp;
-    if (!/^\#/ && !/^\s*$/) {	# not comment or blank line
-      my $output = `$test_cmd testsuite app case $_`; # run test
-      $msg .= $output . "\n";
+    if (!/^#/ && !/^\s*$/) {	# not comment or blank line
+      $output = `$cmd $_` . '\n';
+      $msg .= $output;
       print LOG $output;
     }
   }
@@ -40,7 +83,7 @@ close LOG;
 # send mail
 my $smtp = Net::SMTP->new('localhost');
 $smtp->mail('noreply');
-$smtp->to('weiting.t.lu@gmail.com');
+$smtp->to($EMAIL_RECIPIENT);
 
 $smtp->data();
 $smtp->datasend($msg);
