3e5747f9a9249cd4f3a40e6d81de12293fc71a9e
John Wang <John Wang@localhost>
2011-05-04 23:57:08 -0700

    fixed some file names to follow naming conventions. Add some skeleton to book model, controller, and views.

---
+++ sharingmedia/app/controllers/books_controller.php
@@ -0,0 +1,29 @@
+<?php
+class BooksController extends AppController {
+	var $name = 'Book';
+
+	function addBook() {
+
+	}
+
+	function deleteBook() {
+
+	}
+
+	function editBook() {
+
+	}
+
+	function showBook($id) {
+
+	}
+
+	function queryGoogle() {
+
+	}
+
+	function querySharingMedia() {
+
+	}
+}
+?>
