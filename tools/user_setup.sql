4ad8fe3a41bde98e2ea07f782ddd063a01b9e9c1
parsonsx <parsonsx@localhost>
2011-05-04 21:24:48 -0700

    edited and renamed database_setup.sql to user_setup.sql, removed the dawgsquad database and libraries table creation, since they are no longer need now that the frontpage is now a page and does not need a model or controller.

--- tools/user_setup.sql
+++ tools/user_setup.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE dawgsquad;
-
-USE dawgsquad;
-
-CREATE TABLE libraries(
-	lib_id int
-);
-
 CREATE USER 'dawgsquad'@'localhost' IDENTIFIED BY 'QWjZcECPAyfQfYcQ';
 GRANT ALL PRIVILEGES ON *.* TO 'dawgsquad'@'localhost' WITH GRANT OPTION; 
 CREATE USER 'dawgsquad'@'%' IDENTIFIED BY 'QWjZcECPAyfQfYcQ';
