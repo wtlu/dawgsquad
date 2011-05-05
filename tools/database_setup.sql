221c9102a5caf8994ebf58800421e85c246c0787
wtlu <wtlu@localhost>
2011-05-04 17:17:53 -0700

    changed database_setup so it now creates proper user accoutns

--- tools/database_setup.sql
+++ tools/database_setup.sql
@@ -6,3 +6,7 @@ CREATE TABLE libraries(
 	lib_id int
 );
 
+CREATE USER 'dawgsquad'@'localhost' IDENTIFIED BY 'QWjZcECPAyfQfYcQ';
+GRANT ALL PRIVILEGES ON *.* TO 'dawgsquad'@'localhost' WITH GRANT OPTION; 
+CREATE USER 'dawgsquad'@'%' IDENTIFIED BY 'QWjZcECPAyfQfYcQ';
+GRANT ALL PRIVILEGES ON *.* TO 'dawgsquad'@'%' WITH GRANT OPTION; 
