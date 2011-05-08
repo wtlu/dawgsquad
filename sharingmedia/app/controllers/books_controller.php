ee2215ac2a734c02a76aa20c0ac75845f9488459
Greg Brandt <brandtg@uw.edu>
2011-05-08 11:59:44 -0700

    removed library test

---
+++ sharingmedia/app/controllers/books_controller.php
@@ -0,0 +1,33 @@
+<?php
+class BooksController extends AppController {
+	var $name = 'Books';
+
+	function add_books() {
+
+	}
+
+	function addBook() {
+
+	}
+
+	function deleteBook() {
+
+	}
+
+	function editBook() {
+
+	}
+
+	function showBook($id) {
+
+	}
+
+	function queryGoogle() {
+
+	}
+
+	function querySharingMedia() {
+
+	}
+}
+?>
---
+++ sharingmedia/app/views/books/add_books.ctp
@@ -0,0 +1,1 @@
+<!-- File: /app/views/posts/add_books.ctp -->	
