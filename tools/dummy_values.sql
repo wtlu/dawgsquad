a89bfa78f82706200cd501ed6296fa5a9f7227a3
parsonsx <parsonsx@localhost>
2011-05-08 11:40:23 -0700

    fixed media_db_setup.sql to have user.id as primary key, also updated dummy_values.sql to use the real facebook ids in all tuples being added, not just the first two. Assumed weiting as user 1, troy as user 2

--- tools/dummy_values.sql
+++ tools/dummy_values.sql
@@ -7,13 +7,13 @@ INSERT INTO users(id, fname, lname, created)
 	VALUES(1263812002, "Troy", "Martin", NOW());
 	
 INSERT INTO book_initial_offers(user_id, book_id, trade_id, duration, price, created)
-	VALUES(2, 1, 1, 5, 50, NOW());
+	VALUES(1263812002, 1, 1, 5, 50, NOW());
 	
 INSERT INTO book_initial_offers(user_id, book_id, trade_id, duration, price, created)
-	VALUES(1, 2, 2, 10, 100, NOW());
+	VALUES(518118311, 2, 2, 10, 100, NOW());
 	
 INSERT INTO book_initial_offers(user_id, book_id, trade_id, duration, price, created)
-	VALUES(2, 3, 3, 10, 100, NOW());
+	VALUES(1263812002, 3, 3, 10, 100, NOW());
 	
 INSERT INTO books(title, author, ISBN, image, summary, created)
 	VALUES("Operating System Concepts", "Abraham Silberschatz", 0470128720, "http://books.google.com/books?id=g710PwAACAAJ&printsec=frontcover&img=1&zoom=1&l=220", "Operating System Concepts, Eighth Edition remains as current and relevant as ever, helping you master the fundamental concepts of operating systems while preparing yourself for today’s emerging developments.", NOW());
@@ -25,10 +25,10 @@ INSERT INTO books(title, author, ISBN, image, summary, created)
 	VALUES("Building Java Programs", "Stuart Reges", 0136091813, "http://ecx.images-amazon.com/images/I/51EEKpJ3DwL._SL500_AA300_.jpg", "Java programming stuff", NOW());
 	
 INSERT INTO loans(owner_id, client_id, book_id, due_date, created)
-	VALUES(2, 1, 1, '2011-06-10', NOW());
+	VALUES(1263812002, 518118311, 1, '2011-06-10', NOW());
 	
 INSERT INTO transactions(owner_id, client_id, book_id, current_id, trade_id, duration, price, status, created)
-	VALUES(2, 1, 1, 1, NULL, 5, NULL, 0, NOW());
+	VALUES(1263812002, 518118311, 1, 518118311, NULL, 5, NULL, 0, NOW());
 	
 INSERT INTO trades(id, book_id, created)
 	VALUES(1, 2, NOW());
