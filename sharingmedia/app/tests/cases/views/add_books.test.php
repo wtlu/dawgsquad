771078d76afa24471a844edbc4d2f8a7bca7d5d3
Greg Brandt <brandtg@uw.edu>
2011-05-09 18:00:23 -0700

    wrote skeleton for add books use case test

---
+++ sharingmedia/app/tests/cases/views/add_books.test.php
@@ -0,0 +1,32 @@
+<?php
+/** 
+ * File: app/tests/cases/views/add_books.test.php
+ *
+ * Tests add books use case, which involves navigating
+ * to the add books page, entering in book information,
+ * selecting a book from a results page, then entering
+ * in desired initial offer information
+ */
+
+class AddBooksTestCase extends CakeWebTestCase {
+
+  /* tests navigation to add books page */
+  function testAddView() {
+    /* from splash page */
+
+    /* from my library page */
+    /* TODO: implement my library */
+  }
+
+  /* tests searching a book and displaying correct results */
+  function testFindView() {
+    
+  }
+
+  /* tests adding initial offer metadata to a book in user's library */
+  function testBookInitialOfferView() {
+    
+  }
+
+}
+?>
