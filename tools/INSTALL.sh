65086e68184825729df4f47ac4c1d678262b171d
Greg Brandt <brandtg@uw.edu>
2011-05-11 14:56:39 -0700

    debugged and tested INSTALL.sh. works on CSE VM. added README with instructions. we should clean up this directory...

--- tools/INSTALL.sh
+++ tools/INSTALL.sh
@@ -30,6 +30,19 @@ yum install mysql mysql-server
 yum install php php-mysql
 /sbin/service httpd restart
 
+# add cake executable to the path
+export PATH=$PATH:/var/www/html/dawgsquad/sharingmedia/cake/console
+
+# set up database using scripts
+mysqladmin password "root"
+echo "Enter mysql root user: "
+read mysql_root_user
+echo "$mysql_root_user password is root"
+echo "Setting up db user..."
+mysql -u $mysql_root_user -p < ./user_setup.sql
+echo "Setting up db tables..."
+mysql -u $mysql_root_user -p mys< ./media_db_setup.sql
+
 # clone the repository to /var/www/html
 yum install mercurial
 cd /var/www/html
@@ -38,19 +51,3 @@ hg clone https://dawgsquad.googlecode.com/hg/ dawgsquad
 # set correct ownership on tmp folder
 cd dawgsquad/sharingmedia
 chown -R `whoami` app/tmp
-
-# add cake executable to the path
-export PATH=$PATH:/var/www/html/dawgsquad/sharingmedia/cake/console
-
-# this modifies L290 of /etc/httpd/conf/httpd.conf
-# to read 'AllowOverride All' instead of 'AllowOverride None'
-perl httpd_patch.pl /etc/httpd/conf/httpd.conf
-
-# set up database using scripts
-echo "Enter mysql root user: "
-read mysql_root_user
-echo "Setting up db user...\n"
-mysql -u $mysql_root_user -p < user_setup.sql
-echo "Setting up db tables...\n"
-mysql -u $mysql_root_user -p < media_db_setup.sql
-
