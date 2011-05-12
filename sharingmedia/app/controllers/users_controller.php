06ba64f559c6290eaf2fbb060ea726939d01653a
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-11 19:45:46 -0700

    still debugging login

--- sharingmedia/app/controllers/users_controller.php
+++ sharingmedia/app/controllers/users_controller.php
@@ -21,9 +21,9 @@ class UsersController extends AppController {
 	function login(){
 		$this->layout = 'login_layout';
 		$this->set('title_for_layout', 'Login');
-		if($this->Session->check('uid')){
+		/*if($this->Session->check('uid')){
 			echo $this->redirect(array('controller'=>'users','action' => 'index'));
-		}
+		}*/
 	}
 	
 	function logout(){
