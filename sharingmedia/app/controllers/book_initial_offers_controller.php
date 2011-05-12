798eaa0eb3a06f863b83bd8a51a6e899a4a613c6
Ken Inoue <ktinoue3@gmail.com>
2011-05-12 02:34:35 -0700

    set up layout for books tabs in my library

--- sharingmedia/app/controllers/book_initial_offers_controller.php
+++ sharingmedia/app/controllers/book_initial_offers_controller.php
@@ -27,9 +27,14 @@ class BookInitialOffersController extends AppController {
 	}
 
 	function my_books($uid){
-		$book_collection = $this->BookInitialOffer->query("SELECT * FROM books, book_initial_offers WHERE books.id = book_id AND user_id = $uid" );
-
-
+		$book_collection = $this->BookInitialOffer->query("SELECT * FROM books, book_initial_offers WHERE books.id = book_initial_offers.book_id AND user_id = $uid");
+		$size = sizeof($book_collection);
+		$trade = array();
+		for($i = 0; $i < $size; $i++){
+			$trade_books[$i]= $this->BookInitialOffer->query("SELECT * FROM books, trades WHERE books.id = trades.book_id and trades.id = " . $book_collection[$i]["book_initial_offers"]["trade_id"]);
+		}
+		$this->set('book_collection', $book_collection);
+		$this->set('trade_books', $trade_books);
 	}
 
    //Called when user presses 'Add Book to My Library' on the initial_offer_details.ctp page, and redirects to the add_books_confirm.ctp page.
