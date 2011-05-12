b18818155042ccd168c2a433f84633af8aabdb14
parsonsx <parsonsx@localhost>
2011-05-12 13:17:01 -0700

    fixed SQL syntax bug in book_initial_offer_controller and updated add books confirmation page to print success or failure of any add.

---
+++ sharingmedia/app/tests/cases/controllers/book_initial_offers_controller.test.php
@@ -0,0 +1,33 @@
+<?php
+class BookInitialOffersControllerTest extends CakeTestCase {
+
+  function startCase() {
+    echo '<h1>Starting Test Case</h1>';
+  }
+  
+  function endCase() {
+    echo '<h1>Ending Test Case</h1>';
+  }
+
+  function startTest($method) {
+    echo '<h3>Starting method ' . $method . '</h3>';
+  }
+
+  function endTest($method) {
+    echo '<hr />';
+  }
+  
+  function testinitial_offer_details(){
+    // Need to implement the test
+  }
+
+  function testmy_books(){
+   // Need to implement the test 
+  }
+
+  function add_books_confirm() {
+    // Need to implement the test 
+  }  
+  
+}
+?>
