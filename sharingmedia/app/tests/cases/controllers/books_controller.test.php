00a8ec3a11602b72116badbe16b0cdfacc1ec368
Tatsuro Oya <oya0306@uw.edu>
2011-05-13 01:37:26 -0700

    finished book_controller.test.php

--- sharingmedia/app/tests/cases/controllers/books_controller.test.php
+++ sharingmedia/app/tests/cases/controllers/books_controller.test.php
@@ -1,24 +1,21 @@
-<!-- File: /app/controllers/book_controller.php -->
-
 <!--
-question left
-1. how to use fixture
-2. how to have user input value in method
+File: /app/tests/cases/books_controller.test.php
 
+	Created: 5/13/2011
+	Author: Tatsuro Oya
 
+	
 -->
-
 <?php
+	
 	/*import controller*/
 	App::import('Controller','Books');
     
-	/* extends controller*/
+	
+	/*extends controller*/
 	class TestBooks extends BooksController {
-		/*do not rendering automatically*/
-		/*let the view render not controller*/
-		/*render is for view*/
-		var $autoRender = false;
-		
+		/*do not rendering from here automatically*/
+		var $autoRender = false;		
 		
 		/* overwirte parent's redirect method if exits
 		 * let redirect direct to redirectURL
@@ -29,22 +26,24 @@ question left
 		}	
 		
 	}
+	
+	
+	/* BookControllerTest here */
 	class BooksControllerTest extends CakeTestCase{
 		var $Books = null;
 		
+
 		
-		/*create instance with TestBooks which is a child of BooksController*/
+		/* Create instance with TestBooks which is a child of BooksController*/
 		function setUp(){
 			$this->Books = new TestBooks();
 			$this->Books->constructClasses();
-		
-		}
-		
-		/*instance check*/	
+		}		
+		 
+		 /* Check if BookController can correctlly creating it's instance */ 	
 		function testBooksControllerInstance(){
 			$this->assertTrue(is_a($this->Books, 'BooksController'));
-			debug("correctlly created instance of Books");
-			
+			debug("checks if correctlly created an instance of Books");	
 		}
 		
 		/*convention for end*/
@@ -72,79 +71,163 @@ question left
 			echo '<hr />';
 		}
 
-		/*functions above checks if initiated correctly or not. 
-		 * the true test function starts from here
+		/* functions above checks if initiated correctly or not. 
+		 * the test functions starts from here
 		 */
 
 		 
-		/*step 1: examines data 
-		 *  checks Book's id =1 's contents
+		/*Test 1: Examines Data used in controller
+		 *  this test checks if the record to be retreived by this controller exists 
+		 *  and checks if it displays expected record
+		 *  
+		 *  read Book's id =1's contents
+		 *  compare with the expected value (we know the dummy var)
 		 *  
 		 */
 		 function testFindData(){
-			/*read Book's id = 1 then store it to the res*/
+			/*read Book's id = 1 then store it to the res1 */
 			$this->Books->Book->id = 1;
-			$res = $this->Books->Book->read();
+			$res1 = $this->Books->Book->read();
+			debug($res1);
+			/* compare with the expected (dummy var) that we prepared */
+			$this ->assertNotNull($res1['Book']);
+			$this ->assertEqual($res1['Book']['id'], 1);
+			$this ->assertEqual($res1['Book']['title'], 'Operating System Concepts');
+			$this ->assertEqual($res1['Book']['author'], 'Abraham Silberschatz');
+			$this ->assertEqual($res1['Book']['ISBN'], '0470128720');
+			$this ->assertEqual($res1['Book']['image'], 'http://books.google.com/books?id=g710PwAACAAJ&printsec=frontcover&img=1&zoom=1&l=220');
 			
-			debug($res);
-			$this ->assertNotNull($res['Book']);
-			$this ->assertEqual($res['Book']['id'], 1);
-		 
 		 }
-		  
-		  
-		/* step 2: examine each Action ( actoin is the view )
+		    
+		
+		
+		/* Test2:  examine each Action function ( actoin is the view )
 		 *
-		 * get the result of "action"
-	 	 * then examines it.
+		 * get the result of "action" then examines it.
 		 *
 		 * 2 important thing:
-		 * array('return' => 'render') is rendering results ( what to be displayes)
-		 * array('return' => 'vars') is the data passes to the views
+		 * array('return' => 'render') is rendering results ( what to be displayed)
+		 * array('return' => 'vars') is the data to be passed to the views  (nothing to be passed in this controller so ignore this)
 		 *
-		 * Plan: examine vars to see if we are passing expected data to the view
-		 * $var = $this->testAction('actionpath', 'return valeu');
+		 * Quick Example: examines vars to see if the controller are passing expected data to the view
+		 * $var = $this->testAction('actionpath', 'return value');
 		 *  
-		 *
+		 */	
+	
+		/* Test2: Action function check
+		 *        for each action we check whether each function is rendering appropriate page 
+		 *        to do this we check page title and layout to see each function is choosing approproate ones 
+		 *		  This can be done by parsing the 1. page title and 2. css 
 		 */
-  
-		/* display what we are passing to the view when add_books_results() gets called*/  
-		function testAdd_books_results() {
-			$result = $this->testAction('/Books/add_books_results', array('return' => 'vars'));
-			debug($result);
-		}		  
-		  
-
-		/*display what is being rendered to view (add_books.ctp) when add_books are called */
-		function testAdd_books() {
+		function testAddBooks() {
+		
+			debug('conducting render check for add_books() action method ');
 			$result = $this->testAction('/Books/add_books', array('return' => 'render'));
-			debug(htmlentities($result));
+			/* expexted page title: add_a_book */
+			/* expexted css: main.css */
+			/* check if they matches by parsing rendering result*/
+			$this->assertPattern("/<title>add_a_book<\/title>/", $result);  
+			$this->assertPattern("/<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/sharingmedia\/app\/webroot\/css\/main.css\" \/>/", $result);
 		}
-		
-		
-		/**/
-		function testAdd_books_resultsFixturized(){
-			
-			$result = $this->testAction('/Books/add_books_results', array('fixturize' => true));
-			
-			
-			debug($result);
-		
 		 
-		 }
+		function testFindBooks() {
+		
+			debug('conducting render check for find_books() action method ');
+			$result = $this->testAction('/Books/find_books', array('return' => 'render'));
+			/* expexted page title: find_a_book */
+			/* expexted css: main.css */
+			/* check if they matches by parsing rendering result*/
+			$this->assertPattern("/<title>find_a_book<\/title>/", $result);  
+			$this->assertPattern("/<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/sharingmedia\/app\/webroot\/css\/main.css\" \/>/", $result);
+		} 
+		
+		function testFindBooksResults() {
+		
+			debug('conducting render check for find_books_results() action method ');
+			$result = $this->testAction('/Books/find_books_results', array('return' => 'render'));
+			
+			/* expexted page title: find_book_result */
+			/* expexted css: main.css */
+			/* check if they matches by parsing rendering result*/
+			$this->assertPattern("/<title>find_book_result<\/title>/", $result);  
+			$this->assertPattern("/<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/sharingmedia\/app\/webroot\/css\/main.css\" \/>/", $result);
+		} 
+		 
+		/* Test3: helper function check
+		 *        checks if query_google() shows the output we expected. 
+		 *		  Here we provide the isbn, author, title for computer network book   
+		 *        Then checks if guery_google() retreive the book we expect
+		 */
 		
 		
-		function testAdd_books_resultsFixturized2() {
+		/* checks if query_google() function returns the expected information for the book */
+		function test_query_google() {
+			App::import('HttpSocket');
+			App::import('Xml');
+			$http = new HttpSocket();
+			$url = 'http://books.google.com/books/feeds/volumes';
+			/*giving test search value*/
+			$results = & new Xml($http->get($url, 'q=isbn:0132126958+inauthor:Tanenbaum+intitle:Computer networks'));
+			$results = Set::reverse($results);
+
+			$google_results = array();
+
+			if ($results['Feed']['totalResults'] > 0) {
+				$google_results = $results['Feed']['Entry'];
+			}
 			
-			$data = array(
-				"Book" =>array(
-					"book_title" => "OS"
-					
-				)
-			);
+			debug( $google_results['Title'][0]['value']);
+			$this->assertEqual($google_results['Title'][0]['value'], 'Computer Networks');
+			return $google_results;
+
+		}
+		 
+		/* checks if get_relevant_data() function appropriately filter out the information of the book obtained by query_google()*/
+		function test_get_relevant_data() {
+
+			$result = $this->test_query_google();
+	
+			$title = $result['Title'][1];
+			# check to see if this book has a second title and add it
+			if (array_key_exists(2, $result['Title'])) {
+				$title = $title . ': ' . $result['Title'][2];
+			}
+			$author = '';
+			# check to see if this book has multiple authors
+			if (array_key_exists('creator', $result)) {
+				$author = $result['creator'];
+			} else if (array_key_exists('Creator', $result)) {
+				$author = $result['Creator'][0];
+				for ($i = 1; $i < count($result['Creator']); $i++) {
+					$author = $author . ', '. $result['Creator'][$i];
+				}
+			}
 			
-			$result = $this->testAction('/Books/add_books_results', array('fixturize' => true, 'data' => $data, 'method' => 'post'));
-			debug($result); 
-		}	
+			$ISBN = str_replace('ISBN:', '', $result['Identifier'][1]);
+			# check to see if this book result has an image, and if not, replace with generic thumbnail
+			if ($result['Link'][0]['type'] == 'image/x-unknown') {
+				$image = $result['Link'][0]['href'];
+				$image = str_replace('zoom=5', 'zoom=1', $image);
+			} else {
+				$image = 'http://books.google.com/googlebooks/images/no_cover_thumb.gif';
+			}
+			$summary = '';
+			if (array_key_exists('description', $result)) {
+				$summary = $result['description'];
+			}
+
+			$relevant_stuff = array('title' => $title, 'author' => $author,
+				'ISBN' => $ISBN, 'image' => $image, 'summary' => $summary);
+
+			$book_sub = array($relevant_stuff);
+			debug($book_sub);
+			$this->assertEqual($book_sub[0]['title'], 'Computer Networks');
+			$this->assertEqual($book_sub[0]['author'], 'Andrew S. Tanenbaum, David Wetherall');
+			$this->assertEqual($book_sub[0]['ISBN'], '0132126958');
+			return $book_sub;
+		}
+
+		
+	
 	}	
 ?>
