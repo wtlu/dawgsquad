feb567e0311cbbd4f8177f9a53808faf8fc6a9a0
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-13 02:53:35 -0700

    typo

--- sharingmedia/app/controllers/users_controller.php
+++ sharingmedia/app/controllers/users_controller.php
@@ -27,7 +27,7 @@ class UsersController extends AppController {
 	function login(){
 		$this->layout = 'login_layout';
 		$this->set('title_for_layout', 'Login');
-		$this->redirect('https://www.facebook.com/dialog/oauth?client_id=218244414868504&redirect_uri=http://localhost/sharingmedia/index.php/users/index');
+		$this->redirect('https://www.facebook.com/dialog/oauth?client_id=218244414868504&redirect_uri=http://apps.facebook.com/sharingmedia/users/index');
 /*		$facebook=new Facebook(array(
 		'appId'=>'218244414868504',
 		'secret'=>'fb83c155cc38febb1fb9024c1a9eb050',
