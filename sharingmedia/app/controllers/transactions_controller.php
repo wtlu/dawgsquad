2049e268264cace6280194340b9ac39e917a8456
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-13 00:04:16 -0700

    adding require add

---
+++ sharingmedia/app/controllers/transactions_controller.php
@@ -0,0 +1,55 @@
+<?php
+
+/* This assumes that the status for the transaction is
+ * 0 - pending; 1 - rejected; 2 - accepted.
+ * We should definitely make that an enum or something later.
+ * This is just quick-and-dirty skeleton code.
+ *  -- Greg
+ *
+ *		Changelog:
+ *		5/12/2011 - John Wang - Added function for accept transaction()
+ */
+
+class TransactionsController extends AppController {
+  var $name = 'Transactions';
+  var $helpers = array('Form', 'Html');
+
+  /* owner agrees on user's proposed medium of exchange
+   * pre: transaction is pending
+   * post: transaction is completed (status == 2)
+   */
+  function acceptTransaction($tid) {
+    /* get the transaction */
+    $t = $this->Transaction->query("SELECT * FROM transactions WHERE id = $tid");
+
+    /* do all the update stuff */
+
+    /* post info to view for updating / test */
+    $this->set('accept_info', $t[0]);
+
+  }
+
+  /* updates the current offer
+   * pre: transaction is pending
+   * post: this->trade_id OR this->duration OR this->price updated
+   */
+  function counterTransaction($tid, $type, $offer) {
+    /* get the transaction */
+    $t = $this->Transaction->query("SELECT * FROM transactions WHERE id = $tid");
+
+    /* do all the update stuff */
+
+    /* post info to view for updating / test */
+    $this->set('counter_info', $t[0]);
+  }
+
+  /* displays user's current transactions */
+  function myTransactions($id = null) {
+
+  }
+  function accept_transaction() {
+		$this->layout = 'main_layout';
+		$this->set('title_for_layout', 'accept transaction');
+  }
+}
+?>
---
+++ sharingmedia/app/tests/cases/views/add_books.test.php
@@ -0,0 +1,74 @@
+<?php
+/** 
+ * File: app/tests/cases/views/add_books.test.php
+ *
+ * Tests add books use case, which involves navigating
+ * to the add books page, entering in book information,
+ * selecting a book from a results page, then entering
+ * in desired initial offer information
+ */
+
+class AddBooksTestCase extends CakeWebTestCase {
+
+  /* tests navigation to add books page */
+  function testAddView() {
+    /* setup */
+    $init_page = 'http://localhost/dawgsquad/sharingmedia/';
+    $final_page = 'http://localhost/dawgsquad/sharingmedia/index.php/books/add_books';
+
+    /* on splash page */
+    $this->get($init_page);
+
+    /* go to add books page */
+    $this->clickLinkById('add_book_link');
+
+    /* make sure on add books page */
+    $this->assertEqual($this->getUrl(), $final_page);
+  }
+
+  /* tests searching a book and displaying correct results 
+   * -- look for operating systems book by ISBN */
+  function testFindAndInitialOfferView() {
+    /* setup */
+    $add_page		= 'http://localhost/dawgsquad/sharingmedia/index.php/books/add_books';
+    $results_page	= 'http://localhost/dawgsquad/sharingmedia/index.php/books/add_books_results';
+    $init_offer_page	= 'http://localhost/dawgsquad/sharingmedia/index.php/book_initial_offers/initial_offer_details';
+    $confirm_page	= 'http://localhost/dawgsquad/sharingmedia/index.php/book_initial_offers/add_books_confirm';
+
+    /* on find page */
+    $this->get($add_page);
+    
+    /* fill out form */
+    $this->setFieldById('BookIsbn', '978-0-470-12872-5');
+
+    /* click submit --> go to results page */
+    $this->click('Continue');
+
+    /* make sure on results page */
+    $this->assertEqual($this->getUrl(), $results_page);
+
+    /* check if our book is on the page */
+    $this->assertText('Operating system concepts');				/* title */
+    $this->assertText('Abraham Silberschatz, Peter B. Galvin, Greg Gagne');	/* authors */
+
+    /* now select a book (doesn't matter what one necessarily...) */
+    $this->setFieldById('choose_book', 'selected');
+    $this->click('Continue');
+
+    /* make sure on initial offer page */
+    $this->assertEqual($this->getUrl(), $init_offer_page);
+
+    /* set up and submit initial offer */
+    $this->setFieldById('choose_sell', 'selected'); /* choose sell */
+    $this->setFieldById('BookInitialOfferSellPrice', '100'); /* sell for $100 */
+    $this->click('Add To MyLibrary');
+
+    /* make sure on results page */
+    $this->assertEqual($this->getUrl(), $confirm_page);
+
+    /* check data
+     * TODO: find out what to display... no format yet */
+  }
+
+}
+?>
