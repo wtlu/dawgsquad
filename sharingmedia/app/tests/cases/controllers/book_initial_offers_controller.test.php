aeb3379b07b9712afe3403d8f58e283c1f5de0b0
jedidiah <jedidiah@localhost>
2011-05-13 14:37:02 -0700

    changes to book initial offer controller test

--- sharingmedia/app/tests/cases/controllers/book_initial_offers_controller.test.php
+++ sharingmedia/app/tests/cases/controllers/book_initial_offers_controller.test.php
@@ -9,76 +9,134 @@
 -->
 
 <?php
+	/*import controller*/
+	App::import('Controller','BookInitialOffers');
 
-  //App::import('Controller','BookInitialOffers');
+	/*extends controller*/
+	class TestBookInitialOffers extends BookInitialOffersController {
+		/*do not rendering from here automatically*/
+		var $autoRender = false;		
+		
+		/* overwirte parent's redirect method if exits
+		 * let redirect direct to redirectURL
+		 * can do test without doing actual redirect
+		 */
+		function redirect($url, $status = null, $exit = true){
+			$this->redirectUrl = $url;
+		}	
+		
+	}
+       
+       /* BookInitialOfferControllerTest Code */
+       class BookInitialOffersControllerTest extends CakeTestCase {
+	             var $BookInitialOffers = null;
+				
+		/* Create instance with TestBookInitialOffers which is a child of BookInitialOffersController*/
+		function setUp(){
+			$this->BookInitialOffers = new TestBookInitialOffers();
+			$this->BookInitialOffers->constructClasses();
+		}		
+		 
+		 /* Check if BookInitialOffersController can correctlly creating it's instance */ 	
+		function testBookInitialOffersControllerInstance(){
+			$this->assertTrue(is_a($this->BookInitialOffers, 'BookInitialOffersController'));
+			debug("checks if correctlly created an instance of BookInitialOffers");	
+		}
+		
+		/*convention for end*/
+		function tearDown(){
+			unset($this->BookInitialOffers);
+		}
 
-
-class BookInitialOffersControllerTest extends CakeTestCase {
-
-  function startCase() {
-    echo '<h1>Starting Test Case</h1>';
-  }
+		/* The next 4 methods are just left at default state, no changes */
+		function startCase() {
+		  echo '<h1>Starting Test Case</h1>';
+		}
   
-  function endCase() {
-    echo '<h1>Ending Test Case</h1>';
-  }
+		function endCase() {
+		  echo '<h1>Ending Test Case</h1>';
+		}
 
-  function startTest($method) {
-    echo '<h3>Starting method ' . $method . '</h3>';
-  }
-
-  function endTest($method) {
-    echo '<hr />';
-  }
+		function startTest($method) {
+		  echo '<h3>Starting method ' . $method . '</h3>';
+		}
+		
+		function endTest($method) {
+		  echo '<hr />';
+		}
   
-  // Testing the inital_offer_details function 
-  function testinitial_offer_details(){
+		/* Testing the inital_offer_details function */
+		function testinitial_offer_details(){
+		  
+		  $result= $this->testAction('/book_initial_offers/initial_offer_details/',
+					     array('return' => 'vars'));
+		  debug($result);
+		  
+		  $this->assertEqual($result['title_for_layout'], 'initial_of_details');
+		}
+		
+		/* Testing the my_books function */
+		function testmy_books(){
+		  
+		  //Testing the my_books that are already in the database (dummy_values.sql)
+		  $result = $this->testAction('/book_initial_offers/my_books/1263812002',
+					      array('return' => 'vars'));
+		  debug($result);
+		  
+		  $this->assertEqual($result['title_for_layout'], 'My Books');
+		  
+		  debug($result['book_collection']);
+		  
+		  // Assert that results obtained from my books has the appropriate data values
+		  /*$this->assertEqual($result['book_collection'][0]['books']['title'], 'Continuous Media Databases');
+		   $this->assertEqual($result['book_collection'][0]['books']['author'], 'Abraham Silberschatz');
+		   $this->assertEqual($result['book_collection'][0]['books']['ISBN'], '9780792378181');
+		   
+		   $this->assertEqual($result['trade_books'][0][0]['trades']['book_id'], 1);
+		   $this->assertEqual($result['trade_books'][0][0]['books']['id'], 1);
+		   
+		   // Code below can be used to test other book_id's in the book_initial_offers table
+		   // Just change the last field of the first parameter of testAction to the new book_id
+		   // Currently we only use the test above to test the dummy values in the DB
+		   /*$result2 = $this->testAction('/book_initial_offers/my_books/2',
+		   array('return' => 'vars'));
+		   debug($result2);*/
+		  
+		}
+		
+		/* Testing the remove_confirm function */
+		function testremove_confirm(){
+		}
+		
+		/* Testing the remove function */
+		function testremove(){
+		  
+		}
+		
+		/* Testing the edit function */
+		function testedit(){
 
-    $result = $this->testAction('/book_initial_offers/initial_offer_details',
-				array('return' => 'vars'));
-    //debug($result);
-    // TODO: Need to test the book_choosen
-    // $this->assertEqual($result['book_chosen'], 'initial_of_details');
-    $this->assertEqual($result['title_for_layout'], 'initial_of_details');
-  }
+		}
 
-  function testmy_books(){
-
-    //Testing the my_books that are already in the database (dummy_values.sql)
-    $result = $this->testAction('/book_initial_offers/my_books/1',
-				array('return' => 'vars'));
-    //debug($result);
-    
-    // Assert that results obtained from my books has the appropriate data values
-    $this->assertEqual($result['book_collection'][0]['books']['title'], 'Continuous Media Databases');
-    $this->assertEqual($result['book_collection'][0]['books']['author'], 'Abraham Silberschatz');
-    $this->assertEqual($result['book_collection'][0]['books']['ISBN'], '9780792378181');
-
-    $this->assertEqual($result['trade_books'][0][0]['trades']['book_id'], 1);
-    $this->assertEqual($result['trade_books'][0][0]['books']['id'], 1);
-
-    // Code below can be used to test other book_id's in the book_initial_offers table
-    // Just change the last field of the first parameter of testAction to the new book_id
-    // Currently we only use the test above to test the dummy values in the DB
-    /*$result2 = $this->testAction('/book_initial_offers/my_books/2',
-				array('return' => 'vars'));
-				debug($result2);*/
-
-  }
-
-  function testadd_books_confirm(){
-    // Need to implement the test 
-    $result = $this->testAction('/book_initial_offers/add_books_confirm',
-				array('return' => 'vars'));
-    debug($result);
-  }  
-
-  function testadd_book_to_mylibrary(){
-    // Need to implement the test
-    $result = $this->testAction('/book_initial_offers/add_book_to_mylibrary',
-				array('return' => 'vars'));
-    debug($result);
-  }
-  
-}
+		/* Testing the add_books_confirm */
+		function testadd_books_confirm(){
+		  // Need to implement the test 
+		  $result = $this->testAction('/book_initial_offers/add_books_confirm',
+					      array('return' => 'vars'));
+		  debug($result);
+		}  
+		
+		/* Testing the add_book_to_my_library function */
+		function testadd_book_to_mylibrary(){
+		  // Need to implement the test
+		  $data = array( 'title' => 'Continuous Media Databases', 'author' => 'Abraham Silberschatz', 
+				 'ISBN' => 9780792378181, 'image' => 'http://bks1.books.google.com/books?id=Ba77UV67q40C&printsec=frontcover&img=1&zoom=5&edge=curl',
+				 'offer_type'=> NULL,'offer_value'=> NULL) ; 
+		  
+		  $result = $this->testAction('/book_initial_offers/add_book_to_mylibrary/2/1263812002',
+					      array('return' => 'vars'));
+		  debug($result);
+		}
+		
+       } /*End Test code for BookInitialOfferController */
 ?>
