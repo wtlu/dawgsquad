a60061c0c8e7d693754a31715f8db2211e1d373d
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-14 00:09:21 -0700

    adding example.css

--- sharingmedia/app/controllers/users_controller.php
+++ sharingmedia/app/controllers/users_controller.php
@@ -65,7 +65,46 @@ class UsersController extends AppController {
 	}
 	
 	function example(){
+		$this->layout = 'index_layout';
+		$this->set('title_for_layout', 'Sharing Media');
+
 		
+		$facebook = new Facebook(array(
+  'appId'  => '218244414868504',
+  'secret' => 'fb83c155cc38febb1fb9024c1a9eb050',
+  'cookie' => true,
+));
+
+// We may or may not have this data based on a $_GET or $_COOKIE based session.
+//
+// If we get a session here, it means we found a correctly signed session using
+// the Application Secret only Facebook and the Application know. We dont know
+// if it is still valid until we make an API call using the session. A session
+// can become invalid if it has already expired (should not be getting the
+// session back in this case) or if the user logged out of Facebook.
+$session = $facebook->getSession();
+
+$me = null;
+// Session based API call.
+if ($session) {
+  try {
+    $uid = $facebook->getUser();
+    $me = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+  }
+}
+
+// login or logout url will be needed depending on current user state.
+if ($me) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
+
+// This call will always work since we are fetching public data.
+$naitik = $facebook->api('/naitik');
+
 	}
 
 	function home(){
