c654669ba90d127d09614d4e30e320a2c617ff36
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-14 16:29:33 -0700

    trying facebook.com example

--- sharingmedia/app/controllers/users_controller.php
+++ sharingmedia/app/controllers/users_controller.php
@@ -68,6 +68,25 @@ class UsersController extends AppController {
 	function example(){
 		$this->layout = 'login_layout';
 		$this->set('title_for_layout', 'Login');
+		$app_id = "218244414868504";
+
+     	$canvas_page = "http://ec2-50-18-34-181.us-west-1.compute.amazonaws.com/dawgsquad/sharingmedia/";
+
+     	$auth_url = "http://www.facebook.com/dialog/oauth?client_id=" 
+            . $app_id . "&redirect_uri=" . urlencode($canvas_page);
+
+     	$signed_request = $_REQUEST["signed_request"];
+
+     	list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+     	$data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
+
+     	if (empty($data["user_id"])) {
+            echo("<script> top.location.href='" . $auth_url . "'</script>");
+     	} else {
+            echo ("Welcome User: " . $data["user_id"]);
+     	} 
+
 
     }
 
--- sharingmedia/app/views/users/example.ctp
+++ sharingmedia/app/views/users/example.ctp
@@ -1,18 +1,1 @@
-<script src="http://static.ak.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php" type="text/javascript"></script>
-<script type="text/javascript">
-    FB.init("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "/xd_receiver.htm");
-    window.addEvent('domready',function(){
-        FB_RequireFeatures(["CanvasUtil"], function(){
-                FB.XdComm.Server.init("/xd_receiver.htm");
-        });
-    });
-</script>
-<?php
-
-
-if (FB.Connect) {
-        FB.Connect.showFeedDialog(templateBundle, templateData, null,null,null,FB.RequireConnect.require,null,"Login",null);
-}
-?>
-
 <h1>LOGIN2</h1>
