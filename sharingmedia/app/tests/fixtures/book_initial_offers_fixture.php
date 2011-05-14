31269e8b5af6fc1e3a9852cac51cf514e03c64bd
wtlu <wtlu@localhost>
2011-05-13 17:43:59 -0700

    changed risk

---
+++ sharingmedia/app/tests/fixtures/book_initial_offers_fixture.php
@@ -0,0 +1,31 @@
+<?php
+class BookInitialOffersFixture extends CakeTestFixture {
+  var $name = 'BookInitialOffers';
+
+  /* table definition */
+  var $fields = array(
+		      'user_id' => array('type' => 'integer', 'key' => 'primary'),
+		      'book_id' => array('type' => 'integer'),
+		      'trade_id' => array('type' => 'integer'),
+		      'duration' => array('type' => 'integer'),
+		      'price' => array('type' => 'float'),
+		      'created' => 'datetime',
+		      'modified' => 'datetime'
+		      );
+
+  /* status:
+   *   0 : pending
+   *   1 : rejected
+   *   2 : accepted
+   */
+
+  /* dummy test data */
+  var $records = array(
+		       array ('user_id' => '1', 'book_id' => '10','trade_id' => null,
+			      'duration' => null, 'price' => '100.0',
+			      'created' => '2011-05-08 19:47:00',
+			      'modified' => '2011-05-08 19:47:30'
+			      )
+		       );
+}
+?>
