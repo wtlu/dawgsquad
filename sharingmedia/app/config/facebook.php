97e4155d86a9289ad076316d9e9c3e2d6b8c9751
Greg Brandt <brandtg@uw.edu>
2011-05-13 19:58:07 -0700

    removed outdated README

---
+++ sharingmedia/app/config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/config/facebook.php
+  */
+$config = array(
+  'Facebook' => array(
+  	'appId' => '218244414868504',
+    'apiKey' => '359aefaa3b2440cdc1e986bc87264e51',
+    'secret' => 'fb83c155cc38febb1fb9024c1a9eb050',
+    'cookie' => true,
+    'locale' => 'en_US',
+  )
+);
+?>
