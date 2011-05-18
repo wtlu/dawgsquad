5f19ce54abcd3f3e6af4ccd8ee9ad2661d92c25a
Tatsuro Oya <oya0306@uw.edu>
2011-05-18 01:02:05 -0700

    book controller test working on fixture

--- sharingmedia/app/tests/fixtures/book_fixture.php
+++ sharingmedia/app/tests/fixtures/book_fixture.php
@@ -1,32 +1,45 @@
 <?php
     class BookFixture extends CakeTestFixture {
-		var $name = 'Book';
-		
-		
-		//create fields 
-		var $fields = array(
-			'id' => array('type' => 'integer', 'key' => 'primary'),
-			'title' => array('type' => 'string', 'length' => 45, 'null' => false),
-			'author' => array('type' => 'string', 'length' => 45, 'null' => false),
-			'ISBN' => array('type' => 'string', 'length' => 15, 'null' => false),
-			'image' => array('type' => 'string', 'length' => 150, 'null' => false),
-			'summary' => array('type' => 'text', 'length' => 300, 'null' => false)
-		
-		
-		);
-	
-	
-	/*dummy test records to test with*/
-	/*
-	var $records = array(
-		array ('id' => 1, 
-			   'title' => 'OS',
-			   'author' => 'personA', 
-			   'ISBN' => '11',  
-			   'image' => 'path',
-			   'summary' => 'this is summary'
-		)
-		
-    );
-    }*/
+                var $name = 'Book';
+               
+               
+                //create fields
+                var $fields = array(
+                        'id' => array('type' => 'integer', 'key' => 'primary'),
+                        'title' => array('type' => 'string', 'length' => 45, 'null' => false),
+                        'author' => array('type' => 'string', 'length' => 45, 'null' => false),
+                        'ISBN' => array('type' => 'string', 'length' => 15, 'null' => false),
+                        'image' => array('type' => 'string', 'length' => 150, 'null' => false),
+                        'summary' => array('type' => 'text', 'length' => 300, 'null' => false)
+               
+               
+                );
+       
+       
+				/*dummy test records to test with*/
+				
+				var $records = array(
+						array ('id' => 1,
+								   'title' => 'Web Programming',
+								   'author' => ' Marty Stepp',
+								   'ISBN' => '578012391',  
+								   'image' => 'path',
+								   'summary' => 'this is summary'
+						)
+					   
+				);
+   
+
+
+			   
+
+
+
+
+
+
+
+
+   }
 ?>
+
