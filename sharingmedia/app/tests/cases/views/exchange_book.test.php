ed80e9e4894e7bb5a39f7678003ad210ed2451c4
Greg Brandt <brandtg@uw.edu>
2011-05-18 14:00:20 -0700

    exchange book test: added skeleton code

---
+++ sharingmedia/app/tests/cases/views/exchange_book.test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File: app/tests/cases/views/exchange_book.test.php
+ *
+ * Tests a sample transaction between two users.
+ */
+
+class ExchangeBookTestCase extends CakeWebTestCase {
+
+  /* one user sells a book to another */
+  function testSellBook() {
+
+  }
+
+  /* one user borrows a book from another */
+  function testBorrowBook() {
+
+  }
+
+  /* one user trades books with another */
+  function testTradeBook() {
+
+  }
+
+}
+?>
