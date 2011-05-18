0acb8bb779258d729123e58cb669d2965a2ee778
Greg Brandt <brandtg@uw.edu>
2011-05-18 10:42:43 -0700

    TEST.pl requires cake in path now. Was not working with cron because of relative paths

---
+++ sharingmedia/app/tests/fixtures/book_fixture.php
@@ -0,0 +1,32 @@
+<?php
+    class BookFixture extends CakeTestFixture {
+		var $name = 'Book';
+		
+		
+		//create fields 
+		var $fields = array(
+			'id' => array('type' => 'integer', 'key' => 'primary'),
+			'title' => array('type' => 'string', 'length' => 45, 'null' => false),
+			'author' => array('type' => 'string', 'length' => 45, 'null' => false),
+			'ISBN' => array('type' => 'string', 'length' => 15, 'null' => false),
+			'image' => array('type' => 'string', 'length' => 150, 'null' => false),
+			'summary' => array('type' => 'text', 'length' => 300, 'null' => false)
+		
+		
+		);
+	
+	
+	/*dummy test records to test with*/
+	/*
+	var $records = array(
+		array ('id' => 1, 
+			   'title' => 'OS',
+			   'author' => 'personA', 
+			   'ISBN' => '11',  
+			   'image' => 'path',
+			   'summary' => 'this is summary'
+		)
+		
+    );
+    }*/
+?>
---
+++ sharingmedia/app/views/book_initial_offers/remove_confirm.ctp
@@ -0,0 +1,9 @@
+<?= $this->Html->css('library', NULL, array('inline' => FALSE)); ?>
+<h1>Please Confirm</h1>
+<div id = "remove_unit">
+	<img class = "book_img" src = "<?=$offer[0]["books"]["image"]?>" alt="<?php echo $offer[0]["books"]["title"]?>"/> 
+	<p>Are you sure you want to remove <?=$offer[0]["books"]["title"]?> by <?php echo$offer[0]["books"]["author"]?>?</p>
+</div>
+<?= $this->Html->link('Yes',"http://apps.facebook.com/sharingmedia/book_initial_offers/remove/".$offer[0]["book_initial_offers"]["book_id"], array('id' => 'remove', 'escape' => false)); ?>
+<?= $this->Html->link('Cancel',"http://apps.facebook.com/sharingmedia/book_initial_offers/my_books/", array('id' => 'cancel', 'escape' => false)); ?>
+
