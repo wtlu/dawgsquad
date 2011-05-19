4077c690ab1277eb31d088f921971f12f6f900c6
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-19 01:54:49 -0700

    my loans appears to be fully functional and is now commented

--- sharingmedia/app/controllers/loans_controller.php
+++ sharingmedia/app/controllers/loans_controller.php
@@ -3,14 +3,17 @@
         Author: Ken Inoue
 
         Changelog:
-	5/13/2011 Ken Inoue created added my_loan function
+	5/13/2011 Ken Inoue added my_loan function
+	5/19/2011 Troy Martin: Actually implemented my_loan function and everything else
 -->
 
 <?php
 class LoansController extends AppController {
 	var $helpers = array ('HTML', 'Form', 'Session', 'Facebook.Facebook');
 	var $name = 'Loans';
-
+	
+	// PRE: Called from my library
+	// POST: Display the users current loans
 	function my_loans(){
 		// set up layout
 	    $this->layout = 'main_layout';
@@ -31,8 +34,9 @@ class LoansController extends AppController {
 	    
 	}
 	
+	// PRE: Transfered here from my library if user selects "complete loan". Each book can only be loaned once, in other words, no duplicates.
+	// POST: Transfer control to a confirmation page
 	function complete_loan($book_id, $due_date){
-		//debug($client_name);
 		// set up layout
 	    $this->layout = 'main_layout';
 	    $this->set('title_for_layout', 'Library || My Loans');
@@ -42,16 +46,19 @@ class LoansController extends AppController {
 	    $client_name_array = $this->Loan->query("SELECT name FROM users WHERE facebook_id = " . $client_id);
 	    $client_name = $client_name_array[0]["users"]["name"];
 	    $book_info = $this->Loan->query("SELECT * FROM books WHERE id = " . $book_id);
+	    //pass parameters
 	    $this->set('book_info', $book_info);
 	    $this->set('client_name', $client_name);
 	    $this->set('due_date', $due_date);
 	}
 	
+	// PRE: Transfered here if user confirmed they wanted to complete a loan.
+	// POST: Removes the tuple corresponding to the loan from the loans table.
 	function remove_loan($book_id, $owner_id){
 		// set up layout
 	    $this->layout = 'main_layout';
 	    $this->set('title_for_layout', 'Library || My Loans');
-	    // remove
+	    // remove tuple from loan table
 	    $this->Loan->query("DELETE FROM loans WHERE owner_id = " . $owner_id . " AND book_id = " . $book_id);		
 	}
 }?>
