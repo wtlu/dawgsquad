50229fb1dc24b3c99bb940ec88ebefd888402a3c
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-18 21:52:00 -0700

    starting to work on my_loans

--- sharingmedia/app/controllers/loans_controller.php
+++ sharingmedia/app/controllers/loans_controller.php
@@ -11,7 +11,15 @@ class LoansController extends AppController {
    var $name = 'Loans';
 
 	function my_loans(){
-                $this->layout = 'main_layout';
-                $this->set('title_for_layout', 'Library || My Loans');
+		// set up layout
+	    $this->layout = 'main_layout';
+	    $this->set('title_for_layout', 'Library || My Loans');
+	    
+	    // query database for users loans
+	    $loan_collection = $this->Loan->query("SELECT * FROM loans, WHERE owner_id = ". $this->Session->read('uid'));
+		//pass variables to page
+		debug($loan_collection);
+		//$this->set('book_collection', $book_collection);
+	    
 	}
 }?>
