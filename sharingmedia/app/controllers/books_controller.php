36c43f27f53761e05a816acbf80ca12d42d7d3a1
John Wang <John Wang@localhost>
2011-05-19 21:09:55 -0700

    typo

--- sharingmedia/app/controllers/books_controller.php
+++ sharingmedia/app/controllers/books_controller.php
@@ -55,7 +55,7 @@ class BooksController extends AppController {
                         # query our database to find the book
                         $book_results = $this->Book->query('SELECT DISTINCT books.*, users.*, b_i_o.*
                                 FROM books books, book_initial_offers b_i_o, users users
-	                                WHERE b_i_o.user_id = ' . $this->Session->read('uid') . '
+	                                WHERE b_i_o.user_id != ' . $this->Session->read('uid') . '
 	                                	AND b_i_o.user_id = users.facebook_id
                                         AND b_i_o.book_id = books.id
                                         AND books.title LIKE "%' .$book_title . '%"
