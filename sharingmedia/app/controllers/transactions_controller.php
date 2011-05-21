b368a8463ca5530b40225ef00eec138d40e74f74
parsonsx <parsonsx@localhost>
2011-05-20 17:09:07 -0700

    fixed use of b vs. books in my_transaction

--- sharingmedia/app/controllers/transactions_controller.php
+++ sharingmedia/app/controllers/transactions_controller.php
@@ -168,9 +168,9 @@ class TransactionsController extends AppController {
 		//Get client user id, get their name.
 		$size = sizeof($transaction_collection);
 		for($i=0; $i < $size; $i++){
-			$client_id = $transaction_collection[$i]['transactions']['client_id'];
+			$client_id = $transaction_collection[$i]['t']['client_id'];
 			$client_result = $this->Transaction->query('SELECT * FROM users WHERE facebook_id = ' . $client_id . ' ;');
-			$transaction_collection[$i]['client_name'] = $client_result[0]['users']['name'];
+			$transaction_collection[$i]['client_name'] = $client_result[0]['u']['name'];
 		}
 		
 		//pass variables to page
