5e1e227f3b11eaac586ab6ae9d7c629349d48b2c
jedidiah <jedidiah@localhost>
2011-05-27 16:11:13 -0700

    BIO controller tests updated

--- sharingmedia/app/tests/cases/controllers/book_initial_offers_controller.test.php
+++ sharingmedia/app/tests/cases/controllers/book_initial_offers_controller.test.php
@@ -9,6 +9,7 @@
 	5/11/2011 - Jedidiah Jonathan- Testing for all functions in the book_intial_offers_controller added
 	5/17/2011 - Greg Brandt - Removed support code for redirection, got fixture to work
 	5/18/2011 - Jedidiah Jonathan - Testing completed for feature release
+	5/27/2011 - Jedidiah Jonathan - Test updated to reflect current controller code. Some tests are disabled. Read corresponding notes. 
 */
 
   /* Import the Book Initial Offer controller and Model for testing */
@@ -62,23 +63,45 @@ class BookInitialOffersControllerTest extends CakeTestCase {
     ClassRegistry::flush();
   }
 	     
-  
-  /* Testing the inital_offer_details function */
+  //---------------------------------------------
+  // INITIAL OFFER DETAILS 
+  // --------------------------------------------
+  // Purpose: The available book information is made available to the 
+  //          intial_offer_detail.ctp
+  // Test: This function just grabs the information from the add books 
+  //       results page and sets that book choosen. But since this invloves the 
+  //       interaction between the add book results page, which is a part of integration 
+  //       test and not unit test. A trivial assertion is perfromed.
+  // Comments: If the code is refactored in the future then this function will have the 
+  //           book choosen as argument and then the initial offer details can be calculated from there.  
+  // 
   function testinitial_offer_details(){
     
-    
+    // Call the function  
     $result= $this->testAction('/book_initial_offers/initial_offer_details/',
 			       array('return' => 'vars'));
-    //debug($result);
+    // debug just used to display the result in the test screen
+    // debug($result);
     
     $this->assertEqual($result['title_for_layout'], 'initial_of_details');
   }
   
-  /* Testing the my_books function */
+  //---------------------------------------------
+  // MY BOOKS  
+  // --------------------------------------------
+  // Purpose: Displays all books listed by the user and  the offer details of each books
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id, finds the associated books and the initial offer details
+  //       for each book.
+  // Comments: This test works smoothly as my_books accepts parameters which align with cakePHP unit testing 
+  //         specifications 
+  // 
   function testmy_books(){
 
     // Call the my_books function with user_id 1
     $result = $this->testAction('/book_initial_offers/my_books/1',array('return' => 'vars'));
+
+    // debug just used to display the result in the test screen
     //debug($result);		
     
      $this->assertTrue(!empty($result));
@@ -101,13 +124,23 @@ class BookInitialOffersControllerTest extends CakeTestCase {
 		  $this->assertEqual($result['book_collection'][0]['book_initial_offers']['created'], '2011-05-08 19:47:00');
 		  $this->assertEqual($result['book_collection'][0]['book_initial_offers']['modified'], '2011-05-08 19:47:30');		  
   }
-  
-  /* Testing the remove_confirm function */
+
+  //---------------------------------------------
+  // REMOVE CONFIRM  
+  // --------------------------------------------
+  // Purpose: Confirmation page called when selecting remove from my library page.
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, finds the associated books and the initial offer details
+  //       for each book.
+  // Comments: This test works smoothly as remove_confirm accepts parameters which align with cakePHP unit testing 
+  //         specifications 
+  // 
   function testremove_confirm(){
     
     // Call the remove_confirm fucntion with the user_id 1 and book_id 10
     $result= $this->testAction('/book_initial_offers/remove_confirm/1/10',
 			       array('return' => 'vars'));
+    // debug just used to display the result in the test screen
     //debug($result);
 
      $this->assertTrue(!empty($result));
@@ -132,15 +165,32 @@ class BookInitialOffersControllerTest extends CakeTestCase {
    
 
   }
-  
-  /* Testing the remove function */
+
+  //---------------------------------------------
+  // REMOVE 
+  // --------------------------------------------
+  // Purpose: Book is removed from users library
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, and removes them from the database. 
+  // Comments: This test has been disabled as the controller code calls the redirect and so the test page suffers with 
+  //            redirect issues. 
+  // 
   function testremove(){
     
-    //$result= $this->testAction("/book_initial_offers/remove/1/10");
-    //debug($result);
+    //$result= $this->testAction('/book_initial_offers/remove/1/10', 
+    //array('return' => 'vars'));
+  //debug($result);
   }
-  
-  /* Testing the edit function */
+
+  //---------------------------------------------
+  // EDIT
+  // --------------------------------------------
+  // Purpose: Lets the user edit initial offer on the my library page
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, displays the results after the edit. 
+  // Comments: This test works smoothly as remove_confirm accepts parameters which align with cakePHP unit testing 
+  //            specifications  
+  //
   function testedit(){
     
     // Call the edit function with user_id 1 and book_id 10
@@ -161,7 +211,15 @@ class BookInitialOffersControllerTest extends CakeTestCase {
     
   }
 
-  /* Testing the edit_confirm function */
+  //---------------------------------------------
+  // EDIT CONFIRM 
+  // --------------------------------------------
+  // Purpose: Simple confirmation page for the user to make sure the user is satisfied with the edit
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, displays confirmation results. 
+  // Comments: This test works smoothly as remove_confirm accepts parameters which align with cakePHP unit testing 
+  //            specifications  
+  //
   function testedit_confirm(){
 
     // Call edit_confirm with user id 1 and book id 10 
@@ -178,56 +236,78 @@ class BookInitialOffersControllerTest extends CakeTestCase {
 		   $this->assertEqual($result['title'], 'Web Programming');
     
   }
-  
-  /* Testing the add_books_confirm */
-		function testadd_books_confirm(){
-		  
-		  $result = $this->testAction('/book_initial_offers/add_books_confirm',
-					      array('return' => 'vars'));
-		  debug($result);
-		  
-		  // Assert that results obtained from add books confirm to have the appropriate data values
-		  // Cannot access the uid from the test here as it is currently being implemented using the Session. 
-		  // Therefore these results will just be NULL 
-		  
-		  $this->assertEqual($result['title_for_layout'], 'initial_of_details');
-		  
-		  // Expected values is NULL 
-		  $this->assertEqual($result['title'], NULL);
-		  $this->assertEqual($result['author'], NULL);
-		}  
-		
-		/* Testing the add_book_to_my_library function */
-		function testadd_book_to_mylibrary(){
-		  
-	       	  debug('Testing the addbook_to_mylibrary');
-		  debug('NOTE: This test is currently disabled as such a test would need to run a major refactor on the code espcially the 
-                         function signature and its body , Read comments in the test code');
-		  // NOTE: THE TESTS BELOW HAVE BEEN COMMENTED OUT AS THE INTERFACE FOR THIS FUNCTION HAS NOT BEEN DEFINED IN ACCORDANCE WITH 
-		  // PASSING PARAMETERS WHICH IS VERY SIGNIFICANT FOR TESTING
-		  // THIS REQUIRES A REFACTOR IN THE FUNCTION SIGNATURE AND BODY; AND THE TEAM HAS DECIDED TO POSTPONE (or DOCUMENT) THE 
-		  // REFACTOR PLAN FOR LATER.
 
-		  /*$title= 'Continuous Media Databases';
-		  $author= 'Abraham Silberschatz';
-		  $ISBN= 9780792378181;
-		  $image= 'http://bks1.books.google.com/books?id=Ba77UV67q40C&printsec=frontcover&img=1&zoom=5&edge=curl';
-		  $offer_type= NULL;
-		  $offer_value= NULL;
+  //---------------------------------------------
+  // ADD BOOKS CONFIRM 
+  // --------------------------------------------
+  // Purpose: Simple confirmation page for the user to make sure the user is satisfied with the books they are adding. 
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, displays confirmation results. 
+  // Comments: This test has been disabled as the controller code does not pass the parameters to the functions
+  //          and this does not help in testing. Read the note below.
+  // 
+ function testadd_books_confirm(){
 		  
-		  $result = $this->testAction('/book_initial_offers/1/$title/$author/$ISBN/$image/$offer_type/$offer_value',
-					      array('return' => 'vars'));
-		  debug($result);
+   debug('Testing the add_book_confirm');
+		 
+   // NOTE: THE TESTS BELOW HAVE BEEN COMMENTED OUT AS THE INTERFACE FOR THIS FUNCTION HAS NOT BEEN DEFINED IN ACCORDANCE WITH 
+   // PASSING PARAMETERS WHICH IS VERY SIGNIFICANT FOR TESTING
+   // THIS REQUIRES A REFACTOR IN THE FUNCTION SIGNATURE AND BODY; AND THE TEAM HAS DECIDED TO POSTPONE (or DOCUMENT) THE 
+   // REFACTOR PLAN FOR LATER.
+   
+   /*$result = $this->testAction('/book_initial_offers/add_books_confirm',
+    array('return' => 'vars'));
+    debug($result);
+    
+    // Assert that results obtained from add books confirm to have the appropriate data values
+    // Cannot access the uid from the test here as it is currently being implemented using the Session. 
+    // Therefore these results will just be NULL 
+    
+    $this->assertEqual($result['title_for_layout'], 'initial_of_details');
+    
+    // Expected values is NULL 
+    $this->assertEqual($result['title'], NULL);
+    $this->assertEqual($result['author'], NULL);*/
+ }  
+
+  //---------------------------------------------
+  // ADD BOOKS TO MUY LIBRARY
+  // --------------------------------------------
+  // Purpose: Add the book to the users library. 
+  // Test: This test uses the fixtures: book_fixture and the book_initial_offer_fixture 
+  //       and for a given user id and book id, displays confirmation results. 
+  // Comments: This test has been disabled as the controller code does not pass the parameters to the functions
+  //          and this does not help in testing. Read the note below.
+  // 
+ function testadd_book_to_mylibrary(){
+   
+   debug('Testing the addbook_to_mylibrary');
+   
+   // NOTE: THE TESTS BELOW HAVE BEEN COMMENTED OUT AS THE INTERFACE FOR THIS FUNCTION HAS NOT BEEN DEFINED IN ACCORDANCE WITH 
+   // PASSING PARAMETERS WHICH IS VERY SIGNIFICANT FOR TESTING
+   // THIS REQUIRES A REFACTOR IN THE FUNCTION SIGNATURE AND BODY; AND THE TEAM HAS DECIDED TO POSTPONE (or DOCUMENT) THE 
+   // REFACTOR PLAN FOR LATER.
+   
+   /*$title= 'Continuous Media Databases';
+    $author= 'Abraham Silberschatz';
+    $ISBN= 9780792378181;
+    $image= 'http://bks1.books.google.com/books?id=Ba77UV67q40C&printsec=frontcover&img=1&zoom=5&edge=curl';
+    $offer_type= NULL;
+    $offer_value= NULL;
+    
+    $result = $this->testAction('/book_initial_offers/1/$title/$author/$ISBN/$image/$offer_type/$offer_value',
+    array('return' => 'vars'));
+    debug($result);
+    
+    // Expected output to be NULL since we are adding a book into a user's library. But we have no way to change the uid for this session.
+    $this->assertEqual($result['title'], NULL);
+    $this->assertEqual($result['author'], NULL);
+    $this->assertEqual($result['ISBN'], NULL);
+    $this->assertEqual($result['image'], NULL);*/
 		  
-		  // Expected output to be NULL since we are adding a book into a user's library. But we have no way to change the uid for this session.
-		  $this->assertEqual($result['title'], NULL);
-		  $this->assertEqual($result['author'], NULL);
-		  $this->assertEqual($result['ISBN'], NULL);
-		  $this->assertEqual($result['image'], NULL);*/
-		  
-		}
+ }
+ 
 		
-		
-		
+ 
 } /*End Test code for BookInitialOfferController */
 ?>
