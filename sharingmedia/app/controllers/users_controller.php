51beef200da398259baa092f2324b8e1bcdfed81
tmartin seattle <tmartin.seattle@gmail.com>
2011-05-27 14:45:47 -0700

    got rid of commented out code and aux functions not being used anymore

--- sharingmedia/app/controllers/users_controller.php
+++ sharingmedia/app/controllers/users_controller.php
@@ -9,37 +9,6 @@ class UsersController extends AppController {
 	var $helpers = array ('HTML', 'Form', 'Session', 'Facebook.Facebook');
 	var $name = 'Users';
 	
-	
-/*
-	Pre: Called to display the splash page index.ctp. A user must be logged in otherwise the page is redirected to login.ctp.
-	Post: If a user is new to the app, the user will be added to the users table.
-		The user will see the index page displaying a personal welcome message.
-*/	
-	function index2() {
-		// get the user id and name to see if they are in the users table
-		$user_id = $this->Session->read('uid');
-		$user_name = $this->Session->read('username');
-			
-		// query the table to see if the user is in the table
-		$count = $this->User->query('SELECT COUNT(*) FROM users WHERE facebook_id ="' . $user_id . '";');
-		$count_num = $count[0][0]['COUNT(*)'];
-		
-		//if they aren't in the table, add them
-		if($count_num == 0){
-			$this->User->query('INSERT INTO users(name, password, facebook_id, created) VALUES("' . $user_name . '", null, "' . $user_id . '", NOW());');
-		}
-		
-		// display the correct layout
-		$this->layout = 'index_layout';
-		$this->set('title_for_layout', 'Sharing Media');
-		
-		// check to see if the user is logged out, if so, redirect to login
-		if(!$this->Session->check('uid')){
-			echo $this->redirect(array('controller'=>'users','action' => 'login'));
-		}
-	}
-
-
 /*
 	Pre: Called to display the splash page index.ctp. A user must be logged in otherwise the page is redirected to login.ctp.
 	Post: If a user is new to the app, the user will be added to the users table.
@@ -49,7 +18,7 @@ class UsersController extends AppController {
 		// display the correct layout
 		$this->layout = 'index_layout';
 		$this->set('title_for_layout', 'Sharing Media');
-//		App::import('Vendor', 'facebook');
+		// construct facebook object for getting the session
 		$facebook = new Facebook(array(
   			'appId'  => '218244414868504',
   			'secret' => 'fb83c155cc38febb1fb9024c1a9eb050',
@@ -69,7 +38,6 @@ class UsersController extends AppController {
 		// test if we have a session, otherwise, redirect to login url, which handles asking the user for permission to their info when adding the app 
 		if ($session) {
 	  		try {
-//	    		$uid = $facebook->getUser();
 	    		$me = $facebook->api('/me');
 	    		$user_id = $me['id'];
 	    		$user_name = $me['name'];
@@ -80,8 +48,6 @@ class UsersController extends AppController {
 			      foreach ($friends as $friend) {
 			         $id = $friend['id'];
 			         $friendsArray["$id"] = "foo";
-			         //$i++;
-			         //$name = $friend['name'];
 			      }
 			   	}
 		   		$this->Session->write('friends', $friendsArray);
@@ -96,17 +62,11 @@ class UsersController extends AppController {
 	  		} catch (FacebookApiException $e) {
 	    		error_log($e);
 	  		}
-	} /*else {
-    		echo("<script> top.location.href='" . $loginUrl . "'</script>");
-		}*/
+		}
+		// if the Session doesn't have a 'uid', redirect to login
 		if(!$this->Session->check('uid')){
 			echo $this->redirect(array('controller'=>'users','action' => 'login'));
 		}
-	}
-	
-	function blank(){
-		$this->layout = 'login_layout';
-		$this->set('title_for_layout', 'Login');
 	}
 	
 /*
@@ -133,7 +93,6 @@ class UsersController extends AppController {
 		));
 
 		// initialize new session, get login url
-//		$session = $facebook->getSession();
 		$loginUrl=$facebook->getLoginUrl(array(
 			'canvas'=>1,
 			'fbconnect'=>0,
@@ -141,23 +100,8 @@ class UsersController extends AppController {
 			'next'=>'http://apps.facebook.com/sharingmedia/',
 			'cancel_url'=>'http://www.facebook.com/'
 		));
-//		$me = null;
-		// test if we have a session, otherwise, redirect to login url, which handles asking the user for permission to their info when adding the app 
-/*		if ($session) {
-	  		try {
-	    		$uid = $facebook->getUser();
-	    		$me = $facebook->api('/me');
-	  		} catch (FacebookApiException $e) {
-	    		error_log($e);
-	  		}
-		} else {*/
-	   		echo("<script> top.location.href='" . $loginUrl . "'</script>");	
-//		}
-/*
-		if($this->Session->check('uid')){
-			echo $this->redirect(array('controller'=>'users','action' => 'index'));
-		}
-*/
+		// redirect to login if Session doesn't have 'uid' set or ask user for permissions if they haven't added the app
+	   	echo("<script> top.location.href='" . $loginUrl . "'</script>");	
 	}
 
 /*
