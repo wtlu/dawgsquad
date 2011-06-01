7726ad7f09656bf333ba1360ebd1c59dfef57f95
parsonsx <parsonsx@localhost>
2011-06-01 16:10:47 -0700

    models now have proper comments, as per TA feedback

--- sharingmedia/app/models/transaction.php
+++ sharingmedia/app/models/transaction.php
@@ -1,5 +1,9 @@
 <?php
 
+//Model Definition for Transaction
+//Provides access to database for transactions controller user.
+//Required by cakePHP framework.
+
 class Transaction extends AppModel {
   var $name = 'Transaction';
 
--- sharingmedia/app/models/user.php
+++ sharingmedia/app/models/user.php
@@ -1,5 +1,10 @@
 <?php
 
+//Model Definition for User 
+//Provides access to database for users controller user.
+//Required by cakePHP framework.
+
+
 class User extends AppModel {
 	var $name = 'User';
 }
