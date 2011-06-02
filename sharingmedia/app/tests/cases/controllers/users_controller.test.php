68e0c7f7a1730903fe9aa1ef629364aa5c6716d7
Tatsuro Oya <oya0306@uw.edu>
2011-06-01 20:21:59 -0700

    test case for Brause_test_result() and user_controller test still having redirecting issue. Fixing right now.

--- sharingmedia/app/tests/cases/controllers/users_controller.test.php
+++ sharingmedia/app/tests/cases/controllers/users_controller.test.php
@@ -11,11 +11,36 @@ File: /app/tests/cases/users_controller.test.php
 	
 	/*import controller*/
 	App::import('Controller','Users');
+	App::import('Controller','User');
+ 
+ class TestUsersController extends UsersController {
+
+  var $name = 'Users';
+  
+  var $autoRender = false;
+  
+  function redirect($url, $status = null, $exit = true) {
+    $this->redirectUrl = $url;
+  }
+  
+  function render($action = null, $layout = null, $file = null) {
+    $this->renderedAction = $action;
+  }
+  
+  function _stop($status = 0) {
+    $this->stopped = $status;
+  }
+
+}
+
+ 
+ 
+ 
  
 	/* UserControllerTest here */
 	class UsersControllerTest extends CakeTestCase{
 		
-		var $fixtures = array( 'app.user', 'app.book', 'app.book_initial_offer', 'app.transaction' );
+		var $fixtures = array( 'app.user', 'app.book', 'app.book_initial_offer', 'app.transaction', 'app.loan' );
 		var $helpers = array ('HTML', 'Form', 'Session', 'Facebook.Facebook');
 
 
@@ -32,16 +57,38 @@ File: /app/tests/cases/users_controller.test.php
 		function startTest($method) {
 			/*executed before method*/
 			echo '<h3>Starting method ' . $method . '</h3>';
+			$this->Users = new TestUsersController();
+			$this->Users->constructClasses();
+			$this->Users->Component->initialize($this->Users);		
+		
 		}
+		
+		
+		
 
 		function endTest($method) {
 			/*executed after method*/
 			echo '<hr />';
+			 $this->Users->Session->destroy();
+			unset($this->Users);
+			ClassRegistry::flush();
 		}
 
 	
-		function testImpossible(){
-			debug('The User Controller will not be tested because the only functionality it provides is a wrapper around the Facebook login system. For security reasons, Facebook disallows scripting logging in. ');
+		function testIndex(){
+			//debug('The User Controller will not be tested because the only functionality it provides is a wrapper around the Facebook login system. For security reasons, Facebook disallows scripting logging in. ');
+			
+			
+			$this->Users->Session->write('uid', 2);
+			
+			$result = $this->testAction('/Users/index', 
+										array('return' => 'vars')
+				);
+
+			debug($result);	
+		
+			$data = $this->User->find('all');
+		
 		}	
 		
 		
